b34a68d55fb50353da7f809655836d4a8d5a1f37
curioustechizen <techie.curious@gmail.com>
2015-04-04 09:47:48 +0530

    Cleaned up trial strings from main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -4,29 +4,6 @@
 import 'package:LeetTranslator/LeetTranslator.dart' as LeetTranslator;
 
 main() {
-//  print("${LeetTranslator.toLeet("Hello World")}");
-//  print("${LeetTranslator.fromLeet("H3ll0 W0rld")}");
-  
-  String description = '''
-50f7w4r3 3n61n33r5 h4v3 4 p3nch4n7 f0r 50lv1n6 c0mpl3x 4nd 1n73r3571n6 pr0bl3m5. 6006l3 15 much m0r3 7h4n 534rch, 4nd 0ur m15510n h45 much 6r3473r 5c0p3. 70 h4ndl3 1nf0rm4710n 47 7h3 5c4l3 0f 7h3 w3b r3qu1r35 1d345 fr0m ju57 4b0u7 3v3ry 4r34 0f c0mpu73r 5c13nc3, 1nclud1n6 1nf0rm4710n r37r13v4l, 4r71f1c14l 1n73ll163nc3, n47ur4l l4n6u463 pr0c3551n6, d157r1bu73d c0mpu71n6, l4r63-5c4l3 5y573m d3516n, n37w0rk1n6, 53cur17y, d474 c0mpr35510n, u53r 1n73rf4c3 d3516n, 37c.
-Responsibilities
-wr173 53rv3r-51d3 c0d3 f0r w3b-b453d 4ppl1c4710n5, cr3473 r0bu57 h16h-v0lum3 pr0duc710n 4ppl1c4710n5, 4nd d3v3l0p pr0707yp35 qu1ckly.
-bu1ld 0ur pl47f0rm5, 5y573m5 4nd n37w0rk1n6 1nfr457ruc7ur3 u51n6 y0ur 57r0n6 b4ck6r0und 1n d157r1bu73d 5y573m5, 05/k3rn3l, n37w0rk 5y573m d3516n, 4nd l4r63 5c4l3 570r463 5y573m5.
-bu1ld 1n73rn4l 5y573m5 u53d by 7h0u54nd5 0f 6006l3r5 4r0und 7h3 w0rld w17h y0ur d0m41n 3xp3r7153 1n hr, 574ff1n6, l364l, 4nd 4ll 07h3r c0rp0r473 func710n5.
-5p3c14l157 d0m41n5: u1 d3v3l0pm3n7 w17h 4j4x 4nd 51m1l4r 73chn0l06135, cl13n7 4ppl1c4710n d3v3l0pm3n7 f0r w1nd0w5/m4c (chr0m3, 700lb4r, 37c.), 3mb3dd3d 5y573m5 4nd m0b1l3 4pp5 (4ndr01d), d3v3l0p3r 700l5 (1d35, l4r63-5c4l3 bu1ld 5y573m5, c0mp1l3r5), 1n73rn4710n4l1z4710n.
-Minimum qualifications
-3xp3r13nc3 r34d1n6 4nd wr171n6 1n l337.
-b5 1n c0mpu73r 5c13nc3 0r r3l473d 73chn1c4l d15c1pl1n3 0r 3qu1v4l3n7 pr4c71c4l 3xp3r13nc3.
-pr06r4mm1n6 3xp3r13nc3.
-l4r63 5y573m5 50f7w4r3 d3516n 4nd d3v3l0pm3n7 3xp3r13nc3, w17h kn0wl3d63 0f un1x/l1nux.
-Preferred qualifications
-m5 0r phd 1n c0mpu73r 5c13nc3.
-57r0n6 00 5k1ll5.
-4 50l1d f0und4710n 1n c0mpu73r 5c13nc3, w17h 57r0n6 c0mp373nc135 1n d474 57ruc7ur35, 4l60r17hm5, 4nd 50f7w4r3 d3516n.
-c0d1n6 5k1ll5 1n c/c++, j4v4, py7h0n 0r j4v45cr1p7/4j4x, d474b453 d3516n 4nd 5ql, 4nd/0r kn0wl3d63 0f 7cp/1p 4nd n37w0rk pr06r4mm1n6.
-Area
-6006l3 15 4nd 4lw4y5 w1ll b3 4n 3n61n33r1n6 c0mp4ny. w3 h1r3 p30pl3 w17h 4 br04d 537 0f 73chn1c4l 5k1ll5 wh0 4r3 r34dy 70 74ckl3 50m3 0f 73chn0l06y'5 6r347357 ch4ll3n635 4nd m4k3 4n 1mp4c7 0n m1ll10n5, 1f n07 b1ll10n5, 0f u53r5. 47 6006l3, 3n61n33r5 n07 0nly r3v0lu710n1z3 534rch, 7h3y r0u71n3ly w0rk 0n m4551v3 5c4l4b1l17y 4nd 570r463 50lu710n5, l4r63-5c4l3 4ppl1c4710n5 4nd 3n71r3ly n3w pl47f0rm5 f0r d3v3l0p3r5 4r0und 7h3 w0rld. fr0m 4dw0rd5 70 chr0m3, 4ndr01d 70 y0u7ub3, 50c14l 70 l0c4l, 6006l3 3n61n33r5 4r3 ch4n61n6 7h3 w0rld 0n3 73chn0l061c4l 4ch13v3m3n7 4f73r 4n07h3r.
-  ''';
-  
-  print("${LeetTranslator.fromLeet(description)}");
+  print("${LeetTranslator.toLeet("Hello World")}");
+  print("${LeetTranslator.fromLeet("H3ll0 W0rld")}");  
 }
